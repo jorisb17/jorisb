7a70a2257fed362e1ec0550054714c8c2e2a42d2
Joris Bruinsma <joris@jorisb.nl>
2019-11-13 10:20:18 +0100

    Add firebase for hosting

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomepageComponent = () => {
+    return (
+        <div>
+            <h1>Hallo Marijn</h1>
+        </div>
+    );
+};
+
+export default HomepageComponent;
