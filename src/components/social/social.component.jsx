af97d0b8ba866a267abbebb461f1458bd21ac6e2
Joris Bruinsma <joris@jorisb.nl>
2019-11-14 18:32:08 +0100

    finished social links

--- src/components/social/social.component.jsx
+++ src/components/social/social.component.jsx
@@ -4,11 +4,21 @@ import './social.styles.scss'
 const SocialComponent = () => {
     return (
         <div className={'social'}>
+            <div className={'title'}>
+                <h2>Social links</h2>
+            </div>
             <ul style={{"list-style-type": "none"}}>
                 <li>
-                    <div id={'container'}>
-                        <div className={'username'}>Jorisb17</div>
-                    </div>
+                    <a href={'https://github.com/jorisb17'}>
+                        <div className={'container-git'}>
+                            <div className={'username'}>Jorisb17</div>
+                        </div>
+                    </a>
+                    <a href={'https://github.com/jorisb17'}>
+                        <div className={'container-linked'}>
+                            <div className={'username'}>Jorisb17</div>
+                        </div>
+                    </a>
                 </li>
             </ul>
         </div>
