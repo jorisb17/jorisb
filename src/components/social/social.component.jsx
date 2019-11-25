50dfbc4696dd67227199f28018df9cf0da978ff4
Joris Bruinsma <joris@jorisb.nl>
2019-11-25 19:02:00 +0100

    Fixed social links bug where image was low quality

--- src/components/social/social.component.jsx
+++ src/components/social/social.component.jsx
@@ -7,15 +7,17 @@ const SocialComponent = () => {
             <div className={'title'}>
                 <h2>Social links</h2>
             </div>
-            <ul style={{"list-style-type": "none"}}>
-                <li>
+            <ul style={{"list-style-type": "none", "margin": "0"}}>
+                <li style={{"margin": "0"}}>
                     <a href={'https://github.com/jorisb17'}>
-                        <div className={'container-git'}>
+                        <div className={"container-git"}>
+                            <img src={"./assets/GitHub.png"} width={'40px'} height={'auto'} alt={"Github-Logo"}/>
                             <div className={'username'}>Jorisb17</div>
                         </div>
                     </a>
-                    <a href={'https://github.com/jorisb17'}>
+                    <a href={'https://www.linkedin.com/in/joris-bruinsma-11978179/'}>
                         <div className={'container-linked'}>
+                            <img alt="LinkedIn Logo" src={'./assets/LI.png'} width={"40px"} height={"auto"}/>
                             <div className={'username'}>Jorisb17</div>
                         </div>
                     </a>
