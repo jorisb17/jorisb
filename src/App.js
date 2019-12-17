29e2d12ed664fe6a6c90f893f43377b25fbb9946
Joris Bruinsma <joris@jorisb.nl>
2019-12-17 17:00:14 +0100

    Removed firebase from github

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import HomepageComponent from "./pages/homepage/homepage.component";
 import NotFoundComponent from "./pages/not-found/not.found.component";
+import firebase from "firebase";
+import config from './firebase/firebase.utils'
 
-function App() {
-    const About = () => (
+class App extends React.Component {
+
+    componentDidMount() {
+        firebase.initializeApp(config);
+    }
+
+    About = () => (
         <h1>About this example....</h1>
     );
-  return (
-    <div>
-      <Switch>
-        <Route exact path={'/'} component={HomepageComponent}/>
-        <Route path="/about" component={About}/>
-        <Route component={NotFoundComponent}/>
-      </Switch>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path={'/'} component={HomepageComponent}/>
+                    <Route path="/about" component={this.About}/>
+                    <Route component={NotFoundComponent}/>
+                </Switch>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
