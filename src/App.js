0c360d44c2c1da28b6577f14633e3abfcf145d21
Joris Bruinsma <joris@jorisb.nl>
2019-12-17 17:15:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import HomepageComponent from "./pages/homepage/homepage.component";
 import NotFoundComponent from "./pages/not-found/not.found.component";
-import firebase from "firebase";
-import config from './firebase/firebase.utils'
 
 class App extends React.Component {
-
-    componentDidMount() {
-        firebase.initializeApp(config);
-    }
 
     About = () => (
         <h1>About this example....</h1>
